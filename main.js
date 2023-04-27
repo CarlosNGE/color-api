cd028b4dc7d6997f0c53c4b43a4ad30890d6cad0
artdm lab <artdm@dvc.edu>
2023-04-27 10:48:17 -0700

    add array

--- main.js
+++ main.js
@@ -3,10 +3,15 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  fetch("https://www.colr.org/json/color/random")
+  fetch("https://www.colr.org/json/colors/random/7")
   .then(response => response.json()) 
   .then(data => {
-    document.body.style.backgroundColor = `#${data.new_color}`;
+    let myColors = [];
+    data.colors.forEach(item => {
+      myColors.push("#" + item.hex); 
+    });
+    //console.log('colors:', myColors);
+    //document.body.style.backgroundColor = `#${data.new_color}`;
   }).catch(err => {
     console.error('oops', err.message);
   });
